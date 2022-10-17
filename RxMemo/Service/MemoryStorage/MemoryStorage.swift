f8074917cf016761fe4fb8c6a88f6a836954d864
csehuman <paullee20204@snu.ac.kr>
2022-10-17 09:14:24 +0900

    Step 4 - Setup memory storage

---
+++ RxMemo/Service/MemoryStorage/MemoryStorage.swift
@@ -0,0 +1,59 @@
+//
+//  MemoryStorage.swift
+//  RxMemo
+//
+//  Created by Paul Lee on 2022/10/17.
+//
+
+import Foundation
+import RxSwift
+
+class MemoryStorage: MemoStorageType {
+    // 메모리에 메모를 저장.
+    private var list = [
+        Memo(content: "Hello, RxSwift", insertDate: Date().addingTimeInterval(-10)),
+        Memo(content: "Lorem Ipsum", insertDate: Date().addingTimeInterval(-20))
+    ]
+    
+    private lazy var store = BehaviorSubject<[Memo]>(value: list)
+    
+    @discardableResult
+    func createMemo(content: String) -> RxSwift.Observable<Memo> {
+        let memo = Memo(content: content)
+        list.insert(memo, at: 0)
+        
+        store.onNext(list)
+        
+        return Observable.just(memo)
+    }
+    
+    @discardableResult
+    func memoList() -> RxSwift.Observable<[Memo]> {
+        return store
+    }
+    
+    @discardableResult
+    func update(memo: Memo, content: String) -> RxSwift.Observable<Memo> {
+        let updated = Memo(original: memo, updatedContent: content)
+        
+        if let index = list.firstIndex(where: { $0 == memo }) {
+            list.remove(at: index)
+            list.insert(updated, at: index)
+        }
+        
+        store.onNext(list)
+        
+        return Observable.just(updated)
+    }
+    
+    @discardableResult
+    func delete(memo: Memo) -> RxSwift.Observable<Memo> {
+        if let index = list.firstIndex(where: { $0 == memo }) {
+            list.remove(at: index)
+        }
+        
+        store.onNext(list)
+        
+        return Observable.just(memo)
+    }
+}
