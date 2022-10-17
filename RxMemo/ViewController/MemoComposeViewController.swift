fb9c737dc1532c9ab1b9cc33bae1764c5eeecbbe
csehuman <paullee20204@snu.ac.kr>
2022-10-17 14:51:43 +0900

    Step 25 - Create custom extension for keyboard notification

--- RxMemo/ViewController/MemoComposeViewController.swift
+++ RxMemo/ViewController/MemoComposeViewController.swift
@@ -47,16 +47,8 @@ class MemoComposeViewController: UIViewController, ViewModelBindableType {
             .share()
         
         keyboardObservable
-            .withUnretained(contentTextView)
-            .subscribe(onNext: { tv, height in
-                var inset = tv.contentInset
-                inset.bottom = height
-                tv.contentInset = inset
-                
-                var scrollInset = tv.verticalScrollIndicatorInsets
-                scrollInset.bottom = height
-                tv.verticalScrollIndicatorInsets = scrollInset
-            })
+            .toContentInset(of: contentTextView)
+            .bind(to: contentTextView.rx.contentInset, contentTextView.rx.verticalScrollIndicatorInsets)
             .disposed(by: rx.disposeBag)
     }
 
@@ -75,6 +67,16 @@ class MemoComposeViewController: UIViewController, ViewModelBindableType {
         
         if contentTextView.isFirstResponder {
             contentTextView.resignFirstResponder()
+        }
+    }
+}
+
+extension ObservableType where Element == CGFloat {
+    func toContentInset(of textView: UITextView) -> Observable<UIEdgeInsets> {
+        return map { height in
+            var inset = textView.contentInset
+            inset.bottom = height
+            return inset
         }
     }
 }
