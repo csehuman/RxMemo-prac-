94f30f4856ce162e24201a2f94bfb76b039d54f0
csehuman <paullee20204@snu.ac.kr>
2022-10-17 14:37:38 +0900

    Step 23 - Fix compose-edit view problem

--- RxMemo/ViewModel/MemoDetailViewModel.swift
+++ RxMemo/ViewModel/MemoDetailViewModel.swift
@@ -11,7 +11,7 @@ import RxCocoa
 import Action
 
 class MemoDetailViewModel: CommonViewModel {
-    let memo: Memo
+    var memo: Memo
     
     private var formatter: DateFormatter = {
         let f = DateFormatter()
@@ -43,6 +43,7 @@ class MemoDetailViewModel: CommonViewModel {
     func performUpdate(memo: Memo) -> Action<String, Void> {
         return Action { input in
             self.storage.update(memo: memo, content: input)
+                .do(onNext: { self.memo = $0 })
                 .map { [$0.content, self.formatter.string(from: $0.insertDate)] }
                 .bind(onNext: { self.contents.onNext($0) })
                 .disposed(by: self.rx.disposeBag)
@@ -53,7 +54,7 @@ class MemoDetailViewModel: CommonViewModel {
     
     func makeEditAction() -> CocoaAction {
         return CocoaAction { _ in
-            let composeViewModel = MemoComposeViewModel(title: "메모 편집", content:self.memo.content, sceneCoordinator: self.sceneCoordinator, storage: self.storage, saveAction: self.performUpdate(memo: self.memo))
+            let composeViewModel = MemoComposeViewModel(title: "메모 편집", content: self.memo.content, sceneCoordinator: self.sceneCoordinator, storage: self.storage, saveAction: self.performUpdate(memo: self.memo))
             
             let composeScene = Scene.compose(composeViewModel)
             
